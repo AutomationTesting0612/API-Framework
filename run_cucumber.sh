39bdb7c4be23867a0bd27edbf411c86e4452636e
AutomationTesting0612 <automationtesting0612@gmail.com>
2025-07-19 10:17:50 +0530

    fixing docker file issue

--- run_cucumber.sh
+++ run_cucumber.sh
@@ -7,10 +7,9 @@ java -Dserver.port=$PORT -jar "$WORKSPACE"/target/ApiFramwork-0.0.1-SNAPSHOT.jar
 echo "PID: ${pid4}"
 
 # Wait until app responds (customize URL)
-until curl -s http://localhost:$PORT/actuator/health | grep '"status":"UP"'; do
-  echo "Waiting for Spring Boot app..."
-  sleep 5
-done
+STATUS=$(curl -o /dev/null -s -w "%{http_code}\n" http://localhost:$PORT/actuator/health)
+
+echo "STATUS :$STATUS, Exit code: $?"
 
 mvn clean verify
 
